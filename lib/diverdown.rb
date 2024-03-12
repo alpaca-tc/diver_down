a999f90ff49fca34b23e335ec14cfcc1bdf9aa78
alpaca-tc <alpaca-tc@alpaca.tc>
2024-03-12 19:29:23 +0900

    Remove unused configuration

--- lib/diverdown.rb
+++ lib/diverdown.rb
@@ -9,18 +9,4 @@ module Diverdown
   require 'diverdown/definition_store'
   require 'diverdown/indented_string_io'
   require 'diverdown/helper'
-
-  DEFAULT_CONFIGURATION = {
-    output_dir: '/tmp/output',
-  }.freeze
-
-  # @return [Hash]
-  def self.configuration
-    @configuration ||= DEFAULT_CONFIGURATION.dup
-  end
-
-  # @yield [Hash]
-  def self.configure
-    yield(configuration)
-  end
 end
