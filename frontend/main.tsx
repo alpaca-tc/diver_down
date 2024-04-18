c3a70c45047c2a9d56127d0864f0b46dc14abfed
alpaca-tc <alpaca-tc@alpaca.tc>
2024-04-18 15:31:55 +0900

    Rename Home -> DefinitionList

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { path } from './constants/path'
 import { NotFound } from './pages/Errors'
-import { Show as Home } from './pages/Home'
+import { Show as DefinitionList } from './pages/DefinitionList'
 import { Index as LicenseIndex } from './pages/Lincense'
 import { Index as ModuleIndex, Show as ModuleShow } from './pages/Modules'
 import { Index as SourceIndex, Show as SourceShow } from './pages/Sources'
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout isLoading={false} />}>
-          <Route path={path.home()} element={<Home />} />
+          <Route path={path.home()} element={<DefinitionList />} />
           <Route path={path.sources.index()} element={<SourceIndex />} />
           <Route path={path.sources.show(':sourceName')} element={<SourceShow />} />
           <Route path={path.modules.index()} element={<ModuleIndex />} />
