74c69cfe2985f1ad4e429ce8eb7f95fe42680ab9
alpaca-tc <alpaca-tc@alpaca.tc>
2024-04-22 16:29:37 +0900

    normalize_module_name supports only Module parameter

--- lib/diver_down/helper.rb
+++ lib/diver_down/helper.rb
@@ -7,54 +7,14 @@ module DiverDown
     INSTANCE_CLASS_QUERY = Class.instance_method(:class).freeze
     private_constant :INSTANCE_CLASS_QUERY
 
-    # @param obj [Object, String, Module, Class]
-    # @return [String, nil] if obj is anonymous module, return nil
-    def self.normalize_module_name(obj)
-      if String === obj
-        obj
-      else
-        mod = resolve_module(obj)
+    # @param mod [Module]
+    # @return [String, nil] if mod is anonymous module, return nil
+    def self.normalize_module_name(mod)
+      resolved_mod = resolve_module(mod)
 
-        # Do not call the original method as much as possible
-        CLASS_NAME_QUERY.bind_call(mod) ||
-          (mod.name if mod.respond_to?(:name))
-      end
-    end
-
-    # @note The object passed as an argument may be a Proxied BasicObject.
-    # For example, the DSL of FactoryBot's factory will add a new method in response to the invoked method name,
-    # so passed as an argument methods are not called within this method.
-    #
-    # @return [Module, Class]
-    def self.resolve_module(obj)
-      if module?(obj) # Do not call method of this
-        resolve_singleton_class(obj)
-      else
-        k = INSTANCE_CLASS_QUERY.bind_call(obj)
-        resolve_singleton_class(k)
-      end
-    end
-
-    # @param obj [Module, Class]
-    # @return [Module, Class]
-    def self.resolve_singleton_class(obj)
-      if obj.singleton_class?
-        obj.attached_object
-      else
-        obj
-      end
-    end
-
-    # @param obj [Object]
-    # @return [Boolean]
-    def self.module?(obj)
-      Module === obj
-    end
-
-    # @param obj [Object]
-    # @return [Boolean]
-    def self.class?(obj)
-      Class === obj
+      # Do not call the original method as much as possible
+      CLASS_NAME_QUERY.bind_call(resolved_mod) ||
+        (resolved_mod.name if resolved_mod.respond_to?(:name))
     end
 
     # @param str [String]
