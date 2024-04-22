c0a925793e35adbea69816c97036f14d7f4e7f54
alpaca-tc <alpaca-tc@alpaca.tc>
2024-04-22 16:39:22 +0900

    Re-implemented DiverDown::Helper

--- ext/diver_down/diver_down_ext.c
+++ ext/diver_down/diver_down_ext.c
@@ -1,6 +1,58 @@
 #include "ruby/ruby.h"
 
+static VALUE
+dd_helper_module_p(VALUE helper, VALUE obj) {
+	return rb_obj_is_kind_of(obj, rb_cModule);
+}
+
+static VALUE
+dd_helper_class_p(VALUE helper, VALUE obj) {
+	return rb_obj_is_kind_of(obj, rb_cClass);
+}
+
+static VALUE
+dd_helper_resolve_module(VALUE helper, VALUE obj) {
+	if (rb_obj_is_kind_of(obj, rb_cModule)) {
+		return rb_funcall(helper, rb_intern("resolve_singleton_class"), 1, obj);
+	} else {
+		VALUE klass = rb_obj_class(obj);
+		return rb_funcall(helper, rb_intern("resolve_singleton_class"), 1, klass);
+	}
+}
+
+static VALUE
+dd_helper_resolve_singleton_class(VALUE helper, VALUE klass) {
+	if (rb_funcall(klass, rb_intern("singleton_class?"), 0) == Qtrue) {
+	    return rb_class_attached_object(klass);
+	} else {
+		return klass;
+	}
+}
+
+static VALUE
+dd_helper_normalize_module_name(VALUE helper, VALUE mod) {
+	VALUE resolved_mod = rb_funcall(helper, rb_intern("resolve_module"), 1, mod);
+	VALUE mod_name = rb_mod_name(resolved_mod);
+
+	if (mod_name == Qnil) {
+		return rb_funcall(resolved_mod, rb_intern("name"), 0);
+	} else {
+		return mod_name;
+	}
+}
+
+
+void Init_diver_down_helper(void) {
+	VALUE rb_mDiverDown = rb_const_get(rb_cObject, rb_intern("DiverDown"));
+	VALUE rb_mDiverDownHelper = rb_const_get(rb_mDiverDown, rb_intern("Helper"));
+
+	rb_define_singleton_method(rb_mDiverDownHelper, "module?", dd_helper_module_p, 1);
+	rb_define_singleton_method(rb_mDiverDownHelper, "class?", dd_helper_class_p, 1);
+	rb_define_singleton_method(rb_mDiverDownHelper, "resolve_singleton_class", dd_helper_resolve_singleton_class, 1);
+	rb_define_singleton_method(rb_mDiverDownHelper, "resolve_module", dd_helper_resolve_module, 1);
+	rb_define_singleton_method(rb_mDiverDownHelper, "normalize_module_name", dd_helper_normalize_module_name, 1);
+}
+
 void Init_diver_down_ext(void) {
-  /* VALUE rb_mDiverDown = rb_const_get(rb_cObject, rb_intern("DiverDown")); */
-  /* VALUE rb_mDiverDownHelper = rb_const_get(rb_mDiverDown, rb_intern("Helper")); */
+	Init_diver_down_helper();
 }
--- lib/diver_down/helper.rb
+++ lib/diver_down/helper.rb
@@ -2,21 +2,6 @@
 
 module DiverDown
   module Helper
-    CLASS_NAME_QUERY = Module.method(:name).unbind.freeze
-
-    INSTANCE_CLASS_QUERY = Class.instance_method(:class).freeze
-    private_constant :INSTANCE_CLASS_QUERY
-
-    # @param mod [Module]
-    # @return [String, nil] if mod is anonymous module, return nil
-    def self.normalize_module_name(mod)
-      resolved_mod = resolve_module(mod)
-
-      # Do not call the original method as much as possible
-      CLASS_NAME_QUERY.bind_call(resolved_mod) ||
-        (resolved_mod.name if resolved_mod.respond_to?(:name))
-    end
-
     # @param str [String]
     # @return [Module]
     def self.constantize(str)
