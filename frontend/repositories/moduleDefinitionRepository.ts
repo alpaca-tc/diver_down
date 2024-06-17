16a3bab82980ab9660f0f3ce708577d1ac2610e4
alpaca-tc <alpaca-tc@alpaca.tc>
2024-06-17 14:54:46 +0900

    fix lint

--- frontend/repositories/moduleDefinitionRepository.ts
+++ frontend/repositories/moduleDefinitionRepository.ts
@@ -1,9 +1,9 @@
 import { path } from '@/constants/path'
-import { CombinedDefinitionGraphOptions } from '@/models/combinedDefinition'
+import { CombinedDefinitionOptions } from '@/models/combinedDefinition'
 import { fetchCombinedDefinition, stringifyCombinedDefinitionOptions } from './combinedDefinitionRepository'
 import useSWR from 'swr'
 
-export const useModuleDefinition = (moduleNames: string[], graphOptions: CombinedDefinitionGraphOptions) => {
+export const useModuleDefinition = (moduleNames: string[], graphOptions: CombinedDefinitionOptions) => {
   const requestPath = `${path.api.moduleDefinitions.show(moduleNames)}?${stringifyCombinedDefinitionOptions(graphOptions)}`
   const shouldFetch = moduleNames.length > 0
   const { data, isLoading, mutate } = useSWR(shouldFetch ? requestPath : null, fetchCombinedDefinition)
